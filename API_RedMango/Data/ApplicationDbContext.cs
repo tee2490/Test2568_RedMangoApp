2ec5a3d997ae2711fd9ea9c55c87f76379c99a1a
Teeradet <tee2490@hotmail.com>
2025-06-07 14:02:08 +0700

    Create SQL Tables and push Migrations

---
+++ API_RedMango/Data/ApplicationDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace API_RedMango.Data
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+            optionsBuilder.UseSqlServer("Server=Teeradet;Database=Test1RedMangoDB;TrustServerCertificate=True;Trusted_Connection=True;");
+        }
+
+        public DbSet<ApplicationUser> ApplicationUsers { get; set; }
+    }
+}
--- API_RedMango/Program.cs
+++ API_RedMango/Program.cs
@@ -1,3 +1,7 @@
+global using API_RedMango.Data;
+global using API_RedMango.Models;
+using Microsoft.AspNetCore.Identity;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +10,10 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<ApplicationDbContext>();
+builder.Services.AddIdentity<ApplicationUser,IdentityRole>()
+    .AddEntityFrameworkStores<ApplicationDbContext>();
 
 var app = builder.Build();
 
